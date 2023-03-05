f8c68b55c66fc053c725dc807efeca9a28f68d86
Boyan Velikov <boyanvelikov@gmail.com>
2023-03-05 11:09:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function sumEvenNumbers(numbers) {
+  // Initialize a variable to hold the sum of even numbers
+  let sum = 0;
+
+  // Loop through the array and check if each number is even
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      // If the number is even, add it to the sum variable
+      sum += numbers[i];
+    }
+  }
+
+  // Return the sum of even numbers
+  return sum;
+}
